b9e19d8c582e491395b6d0e8625c0e310fa02dc9
leon-do <leondo123@gmail.com>
2020-07-12 13:32:45 -0400

    add company banners

---
+++ Assets/FPS/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    private float startTime;
+    public string foo = "bar";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Time.time - startTime;
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+        string currentTime = "Speed Run: " + minutes + ":" + seconds;
+        PlayerPrefs.SetString("timer", currentTime);
+        timerText.text = PlayerPrefs.GetString("timer");
+    }
+}
---
+++ Assets/WinTimer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTimer : MonoBehaviour
+{
+
+    public Text winTimerText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(PlayerPrefs.GetString("timer"));
+        winTimerText.text = "Time: " + PlayerPrefs.GetString("timer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
