9f672142e7ca17f78809572499a406a29cf355fc
leon-do <leondo123@gmail.com>
2020-09-27 22:13:35 -0400

    Leaderboard working!

--- Assets/FPS/Scripts/Ethereum/HighScore.cs
+++ Assets/FPS/Scripts/Ethereum/HighScore.cs
@@ -1,13 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+using Nethereum.JsonRpc.UnityClient;
+using LeaderboardSolidityContract.Contracts.Leaderboard.ContractDefinition;
 
 public class HighScore : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-       print("helloooo");
+  public void Start()
+  {
+    StartCoroutine(FetchHighScore());
+  }
+
+  private IEnumerator FetchHighScore()
+  {
+    string url = "https://rinkeby.infura.io/v3/7238211010344719ad14a89db874158c";
+    string contractAddress = "0xfeF8684259C1CBf3F436A57D83A5EB78b0D0bfcC";
+    for (int leaderboardIndex = 0; leaderboardIndex < 10; leaderboardIndex++) {
+      var queryRequest = new QueryUnityRequest<LeaderboardFunctionBase, LeaderboardOutputDTOBase>(url, contractAddress);
+      yield return queryRequest.Query(new LeaderboardFunctionBase() { ReturnValue1 = leaderboardIndex }, contractAddress);
+      print(queryRequest.Result.User + queryRequest.Result.Score);
     }
+  }
 
 }
