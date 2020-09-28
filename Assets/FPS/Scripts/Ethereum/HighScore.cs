9d10ecafe34707d38198bfe796846c2d0fa2121a
leon-do <leondo123@gmail.com>
2020-09-27 21:51:25 -0400

    delete hoverbot 2 b/c of nav mesh

---
+++ Assets/FPS/Scripts/Ethereum/HighScore.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       print("helloooo");
+    }
+
+}
