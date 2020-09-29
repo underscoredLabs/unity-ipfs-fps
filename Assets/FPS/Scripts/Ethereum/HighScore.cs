e3987db4dd51a3b16a29b84417b1372a5206d552
leon-do <leondo123@gmail.com>
2020-09-28 21:31:34 -0400

    display high score on lose screen

--- Assets/FPS/Scripts/Ethereum/HighScore.cs
+++ Assets/FPS/Scripts/Ethereum/HighScore.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using Nethereum.JsonRpc.UnityClient;
 using LeaderboardSolidityContract.Contracts.Leaderboard.ContractDefinition;
 
 public class HighScore : MonoBehaviour
 {
+  public Transform highScoreContainer;
+  public Transform highScoreRow;
+  public Text nameText;
+  public Text scoreText;
+
   public void Start()
   {
     StartCoroutine(FetchHighScore());
@@ -14,11 +20,21 @@ public class HighScore : MonoBehaviour
   {
     string url = "https://rinkeby.infura.io/v3/7238211010344719ad14a89db874158c";
     string contractAddress = "0xfeF8684259C1CBf3F436A57D83A5EB78b0D0bfcC";
-    for (int leaderboardIndex = 0; leaderboardIndex < 10; leaderboardIndex++) {
+    for (int leaderboardIndex = 0; leaderboardIndex < 11; leaderboardIndex++)
+    {
+      // fetch leaderboard from eth smart contract
       var queryRequest = new QueryUnityRequest<LeaderboardFunctionBase, LeaderboardOutputDTOBase>(url, contractAddress);
       yield return queryRequest.Query(new LeaderboardFunctionBase() { ReturnValue1 = leaderboardIndex }, contractAddress);
       print(queryRequest.Result.User + queryRequest.Result.Score);
+
+      // display on screen
+      nameText.text = queryRequest.Result.User.ToString();
+      scoreText.text = queryRequest.Result.Score.ToString();
+      Transform entryTransform = Instantiate(highScoreRow, highScoreContainer);
+      RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+      entryRectTransform.anchoredPosition = new Vector2(0, -20 * leaderboardIndex);
     }
+    highScoreRow.gameObject.SetActive(false);
   }
 
 }
