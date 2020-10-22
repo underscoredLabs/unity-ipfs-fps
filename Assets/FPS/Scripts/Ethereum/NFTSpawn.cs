752bada6471ca68dd185c9abd146622cd1785ced
leon-do <leondo123@gmail.com>
2020-10-22 00:53:59 -0400

    optimize monolith

--- Assets/FPS/Scripts/Ethereum/NFTSpawn.cs
+++ Assets/FPS/Scripts/Ethereum/NFTSpawn.cs
@@ -6,8 +6,13 @@ using Opensea.Contracts.ERC721Full.ContractDefinition;
 
 public class NFTSpawn : MonoBehaviour
 {
-  public GameObject monolith;
-  public Transform monolithLocation;
+  // public GameObject monolith;
+  // public Transform monolithLocation;
+
+  public GameObject[] NFTItems;
+
+  public Transform[] NFTLocations;
+
 
   public void Start()
   {
@@ -16,10 +21,6 @@ public class NFTSpawn : MonoBehaviour
 
   private IEnumerator FetchOwnerOf()
   {
-    // tokenId starts at 1
-    GameObject[] NFTItems = { null, monolith };
-    Transform[] NFTLocations = {null, monolithLocation};
-
     string url = "https://rinkeby.infura.io/v3/7238211010344719ad14a89db874158c";
     string contractAddress = "0xcB04BF3E72C5448f1368084Bb312b2F0B2f6529f";
     string userAddress = "0xbC8C6d48aC0D36DE5D4d543b73e49E1Ec512A197";
